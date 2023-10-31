004dc37c22947e8952ef68c847d126e13d11baba
Philip Bäcklund <philipbacklund98@gmail.com>
2023-10-31 19:56:08 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,14 @@
 module "network" {
   source  = "app.terraform.io/phbaorg123/network/google"
   version = "3.4.0"
+  network_name = "philip"
+  project_id = var.project
   # insert required variables here
-subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+    subnets = [
+    {
+      subnet_name   = "gaurav-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+         }
+    ]
 }
